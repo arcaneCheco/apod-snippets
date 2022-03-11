542d184234510539e6b33bb1bb6ced781b4e333e
arcaneCheco <sergio.azizi.14@gmail.com>
2022-03-11 15:11:28 +0000

    chore: vercel config

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
-import dotenv from "dotenv";
-import express, { Request } from "express";
-import Prismic from "@prismicio/client";
-import ResolvedApi from "@prismicio/client/types/ResolvedApi";
-import PrismicDOM from "prismic-dom";
-import UAParser from "ua-parser-js";
+// import dotenv from "dotenv";
+// import express, { Request } from "express";
+// import Prismic from "@prismicio/client";
+// import ResolvedApi from "@prismicio/client/types/ResolvedApi";
+// import PrismicDOM from "prismic-dom";
+// import UAParser from "ua-parser-js";
+// dotenv.config();
+require("dotenv").config();
+const express = require("express");
+const Prismic = require("@prismicio/client");
+const PrismicDOM = require("prismic-dom");
+const UAParser = require("ua-parser-js");
 
-dotenv.config();
 const app = express();
 const PORT = 3000;
 
@@ -14,14 +19,16 @@ app.use(express.static(`${__dirname}/dist`));
 app.set("view engine", "pug");
 app.set("views", `${__dirname}/views`);
 
-const initApi = (req: Request) => {
+// const initApi = (req: Request) => {
+const initApi = (req: any) => {
   return Prismic.getApi(process.env.PRISMIC_ENDPOINT!, {
     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
     req,
   });
 };
 
-const handleRequest = async (api: ResolvedApi) => {
+// const handleRequest = async (api: ResolvedApi) => {
+const handleRequest = async (api: any) => {
   const home = await api.getSingle("home");
 
   const snippets = await api.getSingle("snippets", { fetchLinks: "pod.index" });
@@ -42,7 +49,7 @@ const handleRequest = async (api: ResolvedApi) => {
     textures[snippet.media.name] = snippet.media.url;
   });
 
-  pods.forEach((pod) => {
+  pods.forEach((pod: any) => {
     pod.data.images.forEach((image: any) => {
       image.media.name = image.media.name.split(".")[0];
       textures[image.media.name] = image.media.url;
@@ -67,7 +74,7 @@ const handleRequest = async (api: ResolvedApi) => {
   };
 };
 
-app.use((req, res, next) => {
+app.use((req: any, res: any, next: any) => {
   const ua = UAParser(req.headers["user-agent"]);
 
   res.locals.isDesktop = ua.device.type === undefined;
@@ -79,7 +86,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/", async (req, res) => {
+app.get("/", async (req: any, res: any) => {
   const api = await initApi(req);
   const defaults = await handleRequest(api);
   res.render("views", {
@@ -87,7 +94,7 @@ app.get("/", async (req, res) => {
   });
 });
 
-app.get("/about", async (req, res) => {
+app.get("/about", async (req: any, res: any) => {
   const api = await initApi(req);
   const defaults = await handleRequest(api);
   res.render("views", {
@@ -95,7 +102,7 @@ app.get("/about", async (req, res) => {
   });
 });
 
-app.get("/explore", async (req, res) => {
+app.get("/explore", async (req: any, res: any) => {
   const api = await initApi(req);
   const defaults = await handleRequest(api);
   res.render("views", {
@@ -103,7 +110,7 @@ app.get("/explore", async (req, res) => {
   });
 });
 
-app.get("/detail/:uid", async (req, res) => {
+app.get("/detail/:uid", async (req: any, res: any) => {
   const api = await initApi(req);
   const defaults = await handleRequest(api);
   res.render("views", {
